c74add389e92dfc16a17d8860ca84e68654158a8
Fabricio-Oliveira <fabriciooh22@gmail.com>
2021-09-30 14:45:35 -0300

    adicionar-campos.js

---
+++ adicionar-campos.js
@@ -0,0 +1,15 @@
+const dadosPessoas = {
+    nome: 'Fabiana', 
+    idade: 28,
+    cpf: '0123654789'
+}
+
+console.log(dadosPessoas)
+
+// Adicionando novas chaves em um objeto 
+dadosPessoas.cidade = 'São José'
+
+// Substituindo o valor de uma chave ja existente
+dadosPessoas.nome = 'Theo'
+
+console.log(dadosPessoas)
